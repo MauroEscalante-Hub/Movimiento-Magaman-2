93dd2165f0f93fdea05ec84b3a4eb43d02ef42d2
MauroEscalante-Hub <ms.fangamesclasicos@gmail.com>
2026-04-09 14:03:10 -0300

    Movimiento casi hecho
    Despues de dos dias finalmente consegui algo, ahora crashea cuando intento aplicar gravedad pero ya casi, luego tengo que agregar un dash al personaje, que no deberia ser muy dificil

--- movimiento-2d-megaman-x-intento-2/player.gd
+++ movimiento-2d-megaman-x-intento-2/player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
+var Velocidad: int = 100
+var Gravedad = ProjectSettings.get_setting("Physics/2d/Default_gravity")
+#gravedad para los saltos
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	var input_axis = Input.get_axis("Izquierda","Derecha") 
+	velocity.x = input_axis * Velocidad
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("Saltar") and is_on_floor():
+		velocity.y = -Velocidad
+	velocity.y += Gravedad * delta
+	
 	pass
